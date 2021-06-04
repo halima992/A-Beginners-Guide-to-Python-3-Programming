d86dd79977d20e7e4f3f5cd8303d9641d2b18056
Halima Josh <studentimamu@gmail.com>
2021-06-04 22:01:33 +0300

    Create RepresentingStrings.py
    in this file we will represent string by add single quote or double quotes or multiline string

---
+++ chapter4/RepresentingStrings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  4 21:52:11 2021
+
+@author: Halima
+"""
+print("It's the day") #the output will be --> It's the day
+print('She said "hello" to everyone') #the output will be --> She said "hello" to everyone
+
+
+# =============================================================================
+# The output will be -->
+# Hello
+# 
+# World
+# 
+# =============================================================================
+
+multi_line_sentence = """ 
+Hello
+
+World
+ """
+print(multi_line_sentence)
