45ecc9cbfb11f18e98a5c325239ba8dd3b81a57a
Halima Josh <studentimamu@gmail.com>
2021-06-04 21:32:36 +0300

    create varibles.py
    this code foucs on typy of veriables

---
+++ chapter3/Variables.py
@@ -0,0 +1,16 @@
+#variables with string values
+
+print('Hello, world') 
+name = input('Enter your name: ') 
+print('Hello', name) 
+name = input('What is the name of your best friend: ') 
+print('Hello Best Friend', name)
+
+#variables with diffrent type of value
+my_variable = 'John' 
+print(my_variable) 
+my_variable = 42 
+print(my_variable) 
+my_variable = True 
+print(my_variable)
+
