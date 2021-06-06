e3642e3a5ce812b4d6057c0f75f4ff4cf18f1f6d
Halima Josh <studentimamu@gmail.com>
2021-06-06 22:15:50 +0300

    Create HintsonStrings.py
    python string is senstive . so we can use method like lower() upper() to can compare or do any operation

---
+++ chapter4/HintsonStrings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 21:59:26 2021
+
+@author: Halima
+"""
+# Python Strings Are Case Sensitive
+some_string = 'Hello World'
+some_string.lower().startswith('h')
