b4fb71535724bf79c68b6ce7ef6476ea035f29ba
Halima Josh <studentimamu@gmail.com>
2021-06-18 16:48:26 +0300

    Update 6.e NoneValue.py
    this code explain about None type

--- chapter5/6.e NoneValue.py
+++ chapter5/6.e NoneValue.py
@@ -5,6 +5,14 @@ Created on Fri Jun 18 16:38:10 2021
 @author: Halima
 """
 #  NoneType, with a single value, None
-winner = None
-print(winner is None)
-print(winner is not None)
+winner = None 
+print('winner:', winner) 
+print('winner is None:', winner is None) 
+print('winner is not None:', winner is not None) 
+print(type(winner)) 
+print('Set winner to True') 
+winner = True 
+print('winner:', winner) 
+print('winner is None:', winner is None) 
+print('winner is not None:', winner is not None) 
+print(type(winner))
