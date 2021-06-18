3d3ff8d16664bb49789eefb1c41d707bfdd79c51
Halima Josh <studentimamu@gmail.com>
2021-06-18 16:42:44 +0300

    update
    assignment  with addition operation

--- chapter5/5.d AssignmentOperators.py
+++ chapter5/5.d AssignmentOperators.py
@@ -4,3 +4,6 @@ Created on Fri Jun 18 16:32:48 2021
 
 @author: Halima
 """
+# compound operators
+x = 0 
+x += 1 # has the same behaviour as x = x + 1
---
+++ chapter5/6.e NoneValue.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 18 16:38:10 2021
+
+@author: Halima
+"""
+#  NoneType, with a single value, None
+winner = None
+print(winner is None)
+print(winner is not None)
