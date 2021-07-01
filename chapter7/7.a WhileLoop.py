5dbe5e195f39ff0930d5c2db88122224928fef2b
Halima Josh <studentimamu@gmail.com>
2021-07-01 18:39:15 +0300

    created file of chapter 7
    these codes about loop

---
+++ chapter7/7.a WhileLoop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 20 18:22:10 2021
+
+@author: Halima
+"""
+
+count = 0
+print('Starting') 
+while count < 10: 
+    print(count, ' ', end='') # part of the while loop 
+    count += 1
+print() # not part of the while loop 
+print('Done')
---
+++ chapter7/7.b ForLoop.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  1 18:36:42 2021
+
+@author: Halima
+"""
