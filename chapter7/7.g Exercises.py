09aab325d98ed27389e854ffe34b15abbdcde0f1
Halima Josh <studentimamu@gmail.com>
2021-07-09 21:18:32 +0300

    Update 7.g Exercises.py

--- chapter7/7.g Exercises.py
+++ chapter7/7.g Exercises.py
@@ -36,3 +36,39 @@ if num.isnumeric():
             
 else:
     print('This input is not numarical number')
+
+# =============================================================================
+# Exercise2
+# A Prime Number is a positive whole number, greater than 1,
+# that has no other divisors except the number 1 and the number itself. 
+# That is, it can only be divided by itself and the number 1, 
+# for example thenumbers 2, 3, 5 and 7 are prime numbers as they cannot be divided by 
+# any other whole number. However, the numbers 4 and 6 are not because they can both be divided by the number 2 
+# in addition the number 6 can also be divided by the number 3. 
+# You should write a program to calculate prime number starting from 1 up to the
+# value input by the user. 
+# If the user inputs a number below 2, print an error message. 
+# For any number greater than 2 loop for each integer from 2 to that number and
+# determine if it can be divided by another number 
+# (you will probably need two for loops for this; one nested inside the other). 
+# For each number that cannot be divided by any other number (that is its a prime number) 
+# print it out.
+# =============================================================================
+
+num = input("please Enter a prime number:")
+if num.isnumeric():
+    number= int(num)
+    if number < 2:
+        print('error!')
+    else:
+        
+        for i in range(1,number+1):
+            flag = 0
+            for j in range(2, int(i/2)):
+                if i%j == 0:
+                    flag=flag+1
+                    break
+            if flag == 0:
+                print(i, end = ' ')
+    
+
